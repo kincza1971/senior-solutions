137454b7f4abe2c275145649d86ce4394d1db5a3
kincza <kincza@gmail.com>
2021-06-04 08:47:46 +0200

    MeetingRooms ISSUE missing tests for services resolved              ISSUE wrong assertEquals for lists resolved

--- meetingrooms/src/test/java/MeetingRoomsServiceTestInDatabase.java
+++ meetingrooms/src/test/java/MeetingRoomsServiceTestInDatabase.java
@@ -8,13 +8,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.SQLException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class InDatabaseMeetingRoomsRepositoryTest {
-
+public class MeetingRoomsServiceTestInDatabase {
     InDatabaseMeetingRoomsRepository repo;
+    MeetingRoomsServices services;
     static JdbcTemplate jdbcTemplate;
     static MariaDbDataSource mariaDbDataSource;
+    
 
 
     @BeforeAll
@@ -39,13 +40,14 @@ class InDatabaseMeetingRoomsRepositoryTest {
         flyway.clean();
         flyway.migrate();
         repo =  new InDatabaseMeetingRoomsRepository();
+        services = new MeetingRoomsServices(repo);
         repo.save("Himalája", 5,5);
     }
 
     @Test
     void testSave() {
 
-        List<String> result = repo.getNames();
+        List<String> result = services.getNames();
 
         assertEquals(List.of("Ábrahám","Benjámin","Himalája","Ménrót","Ráhel","Valéria"), result);
     }
@@ -53,7 +55,7 @@ class InDatabaseMeetingRoomsRepositoryTest {
     @Test
     void getNamesReversed() {
 
-        List<String> result = repo.getNamesReversed();
+        List<String> result = services.getNamesReversed();
 
 
         assertEquals(List.of("Valéria","Ráhel","Ménrót","Himalája","Benjámin","Ábrahám"), result);
@@ -61,39 +63,47 @@ class InDatabaseMeetingRoomsRepositoryTest {
 
     @Test
     void getNamesEven() {
-        List<String> result = repo.getNamesEven();
+        List<String> result = services.getNamesEven();
 
         assertEquals(List.of("Benjámin","Ménrót","Valéria"), result);
     }
 
     @Test
     void getAreas() {
-        List<MeetingRoom> resultRooms = repo.getAreas();
+        List<MeetingRoom> resultRooms = services.getAreas();
 
-        assertEquals(new MeetingRoom(2,"Ménrót",12,14,168),resultRooms.get(0));
-        assertEquals(new MeetingRoom(4,"Benjámin",9,9,81),resultRooms.get(1));
-        assertEquals(new MeetingRoom(1,"Ábrahám",7,11,77),resultRooms.get(2));
-        assertEquals(new MeetingRoom(3,"Ráhel",3,4,12),resultRooms.get(5));
+        assertEquals(
+                List.of(
+                        new MeetingRoom(2,"Ménrót",12,14,168),
+                        new MeetingRoom(4,"Benjámin",9,9,81),
+                        new MeetingRoom(1,"Ábrahám",7,11,77),
+                        new MeetingRoom(3,"Valéria", 5,7,35),
+                        new MeetingRoom(5,"Himalája",5, 5,25),
+                        new MeetingRoom(3,"Ráhel",3,4,12)
+                ),
+                resultRooms
+        );
 
     }
 
 
     @Test
     void findByName() {
-        MeetingRoom result = repo.findByName("himaLÁJa");
+        MeetingRoom result = services.findByName("himaLÁJa");
         assertEquals(new MeetingRoom(1,"Himalája",5,5,25),result);
     }
 
     @Test
     void findByNamePart() {
-        List<String> result = repo.findByNamePart("LÁJ");
+        List<String> result = services.findByNamePart("LÁJ");
         assertEquals("{név='Himalája', szélesség=5, Hosszúság=5, terület=25}" + System.lineSeparator(),result.get(0));
     }
 
     @Test
     void findGreater() {
-        List<String> result = repo.findGreater(80);
+        List<String> result = services.findGreater(80);
         assertEquals("{név='Benjámin', szélesség=9, Hosszúság=9, terület=81}" + System.lineSeparator(),result.get(0));
         assertEquals("{név='Ménrót', szélesség=12, Hosszúság=14, terület=168}" + System.lineSeparator(),result.get(1));
     }
+
 }
---
+++ meetingrooms/src/test/java/MeetingRoomsServiceTestInMemory.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeetingRoomsServiceTestInMemory {
+    InMemoryMeetingRoomsRepository repo;
+    MeetingRoomsServices services;
+
+    @BeforeEach
+    void init() {
+        repo = new InMemoryMeetingRoomsRepository();
+        services = new MeetingRoomsServices(repo);
+        services.save("Himalája", 5,5);
+        services.save("Mount Everest", 10,10);
+        services.save("Kékestető", 4,4);
+        services.save("Alpok",6,9);
+    }
+
+    @Test
+    void testSave() {
+
+        List<String> result = services.getNames();
+
+        assertEquals(List.of("Alpok", "Himalája","Kékestető","Mount Everest"),result);
+    }
+
+    @Test
+    void getNamesReversed() {
+
+        List<String> result = services.getNamesReversed();
+
+        assertEquals(List.of("Mount Everest","Kékestető","Himalája","Alpok" ),result);
+    }
+
+    @Test
+    void getNamesEven() {
+        List<String> result = services.getNamesEven();
+
+        assertEquals(List.of("Himalája","Mount Everest"),result);
+    }
+
+    @Test
+    void getAreas() {
+        List<MeetingRoom> resultRooms = services.getAreas();
+
+        assertEquals(
+                List.of(
+                    new MeetingRoom(2,"Mount Everest",10,10,100),
+                    new MeetingRoom(4,"Alpok",6,9,54) ,
+                    new MeetingRoom(1,"Himalája",5,5,25),
+                    new MeetingRoom(3,"Kékestető",4,4,16)
+                ),
+                resultRooms
+        );
+    }
+
+
+    @Test
+    void findByName() {
+        MeetingRoom result = services.findByName("himaLÁJa");
+        assertEquals(new MeetingRoom(1,"Himalája",5,5,25),result);
+    }
+
+    @Test
+    void findByNamePart() {
+        List<String> result = services.findByNamePart("LÁJ");
+        System.out.println(result);
+        assertEquals("{név='Himalája', szélesség=5, Hosszúság=5, terület=25}" + System.lineSeparator(),result.get(0));
+        assertEquals(1,result.size());
+    }
+
+    @Test
+    void findGreater() {
+        List<String> result = services.findGreater(53);
+        assertEquals(
+                List.of(
+                        "{név='Alpok', szélesség=6, Hosszúság=9, terület=54}" + System.lineSeparator(),
+                        "{név='Mount Everest', szélesség=10, Hosszúság=10, terület=100}" + System.lineSeparator()
+                ),
+                result
+        );
+    }
+}
